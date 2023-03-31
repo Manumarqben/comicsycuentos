5d25cfb1e8fb74884e5f3dd67566f6296ddabbc4
marqben <marqben@hotmail.com>
2023-03-31 22:32:03 +0200

    feat: :lipstick: Tema oscuro o claro automático
    Usa el tema oscuro o el claro dependiendo de la configuración del sistema del usuario

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <script src="{{ asset('js/checkTheme.js') }}"></script>
 
         <!-- Styles -->
         @livewireStyles
