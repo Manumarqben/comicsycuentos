8811f0d69229a15acb87a890bc3edf2e93e01430
marqben <marqben@hotmail.com>
2023-04-01 21:10:53 +0200

    feat: :card_file_box: Migración, seed y modelo de aplicantes

---
+++ database/migrations/2023_04_01_190732_create_applicants_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applicants', function (Blueprint $table) {
+            $table->id();
+            $table->string('alias', 50)->unique();
+            $table->string('slug', 80)->unique();
+            $table->foreignId('user_id')->unique()->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applicants');
+    }
+};
---
+++ database/seeders/ApplicantSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ApplicantSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('applicants')->insert([
+            'alias' => 'Applicant',
+            'slug' => str('Applicant')->slug(),
+            'user_id' => User::where('name', 'applicant')->first()->id,
+            'created_at' => now(),
+        ]);
+    }
+}
