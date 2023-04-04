3711507c7b72efe2d3d4d69d189b98658bc9c49c
marqben <marqben@hotmail.com>
2023-04-04 17:49:36 +0200

    feat: :construction: Mostrando información del autor

--- app/Http/Livewire/Author/Show.php
+++ app/Http/Livewire/Author/Show.php
@@ -2,10 +2,20 @@
 
 namespace App\Http\Livewire\Author;
 
+use App\Models\Author;
 use Livewire\Component;
 
 class Show extends Component
 {
+    public Author $author;
+
+    public function mount($slug)
+    {
+        $this->fill([
+            $this->author = Author::where('slug', $slug)->firstOrFail(),
+        ]);
+    }
+
     public function render()
     {
         return view('livewire.author.show');
--- resources/views/livewire/author/show.blade.php
+++ resources/views/livewire/author/show.blade.php
@@ -1,3 +1,22 @@
 <div class="container">
-    {{-- Success is as dangerous as failure. --}}
+    <div id="data"
+        class="flex flex-col sm:flex-row gap-3 items-center sm:items-start pb-8">
+        <div id="profilePhoto">
+            <div
+                class=" h-52 sm:h-64 w-52 sm:w-64 rounded-full overflow-hidden border-4 border-gray-500">
+                <img class="object-cover w-full h-full"
+                    src="{{ $author->profile_photo_path }}"
+                    alt="{{ $author->alias }}" />
+            </div>
+        </div>
+        <div id="info" class="grow space-y-2 px-5">
+            <div id="alias"
+                class="text-2xl sm:text-4xl text-center sm:text-left line-clamp-1">
+                {{ $author->alias }}
+            </div>
+            <div id="biography" class="line-clamp-[9] sm:line-clamp-6 ">
+                <div class="text-justify">{{ $author->biography }}</div>
+            </div>
+        </div>
+    </div>
 </div>
