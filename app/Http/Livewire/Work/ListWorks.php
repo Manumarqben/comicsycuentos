429d09f5bd710b4bc6090d03a4c90c678be0bd56
marqben <marqben@hotmail.com>
2023-04-04 20:15:56 +0200

    feat: :sparkles: filtro por edad en listado de obras

--- app/Http/Livewire/Work/ListWorks.php
+++ app/Http/Livewire/Work/ListWorks.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Work;
 
 use App\Models\Work;
+use Carbon\Carbon;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -26,9 +27,24 @@ class ListWorks extends Component
         $this->state = $state;
     }
 
+    private function childrenFilter()
+    {
+        if (auth()->check()) {
+            $age = Carbon::parse(auth()->user()->birthdate)->age;
+            if ($age >= 18) {
+                return Work::query();
+            }
+        }
+        return Work::whereHas('age', function ($query) {
+            $query->where('year', '<', 18);
+        });
+    }
+
     public function render()
     {
-        $works = Work::where('author_id', $this->author);
+        $works = $this->childrenFilter();
+
+        $works->where('author_id', $this->author);
 
         $works->whereHas('state', function ($query) {
             $query->where('slug', $this->state);
