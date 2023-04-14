dcf5470945252e92dce031cb3d4247112f551502
marqben <marqben@hotmail.com>
2023-04-14 23:38:04 +0200

    feat: :sparkles: Creación de modal, controlador y test

--- app/Http/Livewire/Applicant/Delete.php
+++ app/Http/Livewire/Applicant/Delete.php
@@ -2,10 +2,46 @@
 
 namespace App\Http\Livewire\Applicant;
 
+use App\Models\Applicant;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Livewire\Component;
 
 class Delete extends Component
 {
+    use AuthorizesRequests;
+
+    public $show;
+    public Applicant $applicant;
+
+    protected $rules = [
+        'applicant' => ['required'],
+    ];
+
+    public function mount()
+    {
+        $this->fill([
+            'show' => false,
+            'applicant' => Applicant::where('user_id', auth()->user()->id)->first(),
+        ]);
+    }
+
+    public function delete()
+    {
+        $this->authorize('delete', $this->applicant);
+
+        $this->applicant->delete();
+
+        $this->emit('refresh-navigation-menu');
+        $this->dispatchBrowserEvent('alert', ['message' => 'Application withdrawn']);
+
+        $this->show = false;
+    }
+
+    public function open()
+    {
+        $this->show = true;
+    }
+
     public function render()
     {
         return view('livewire.applicant.delete');
--- tests/Feature/Livewire/Applicant/DeleteTest.php
+++ tests/Feature/Livewire/Applicant/DeleteTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Livewire\Applicant;
 
 use App\Http\Livewire\Applicant\Delete;
+use App\Models\Applicant;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -10,11 +12,33 @@ use Tests\TestCase;
 
 class DeleteTest extends TestCase
 {
-    /** @test */
-    public function the_component_can_render()
-    {
-        $component = Livewire::test(Delete::class);
+    use RefreshDatabase;
 
-        $component->assertStatus(200);
+    // /** @test */
+    // public function the_component_can_render()
+    // {
+    //     $component = Livewire::test(Delete::class);
+
+    //     $component->assertStatus(200);
+    // }
+
+    /** @test */
+    public function a_user_can_withdraw_their_application_to_be_author()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $applicant = Applicant::factory()->create([
+            'user_id' => $user->id,
+        ]);
+
+        $this->assertDatabaseHas('applicants', ['id' => $applicant->id]);
+
+        $component = Livewire::test(Delete::class, ['applicant' => $applicant])
+            ->call('delete')
+            ->assertDispatchedBrowserEvent('alert', [
+                'message' => 'Application withdrawn',
+            ]);
+
+        $this->assertDatabaseMissing('applicants', ['id' => $applicant->id]);
     }
 }
