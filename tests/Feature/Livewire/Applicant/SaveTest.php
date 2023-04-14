22ce37d9885cf3873397b89a97f27be49dd98fca
marqben <marqben@hotmail.com>
2023-04-14 23:52:58 +0200

    Test de save (se me paso en su rama hacerlo)

--- tests/Feature/Livewire/Applicant/SaveTest.php
+++ tests/Feature/Livewire/Applicant/SaveTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Livewire\Applicant;
 
 use App\Http\Livewire\Applicant\Save;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -10,11 +11,27 @@ use Tests\TestCase;
 
 class SaveTest extends TestCase
 {
-    /** @test */
-    public function the_component_can_render()
-    {
-        $component = Livewire::test(Save::class);
+    use RefreshDatabase;
 
-        $component->assertStatus(200);
+    // /** @test */
+    // public function the_component_can_render()
+    // {
+    //     $component = Livewire::test(Save::class);
+
+    //     $component->assertStatus(200);
+    // }
+
+    /** @test */
+    public function a_user_can_apply_to_be_author()
+    {
+        $this->actingAs($user = User::factory()->create());
+
+        $component = Livewire::test(Save::class)
+            ->call('submit')
+            ->assertDispatchedBrowserEvent('alert', [
+                'message' => 'Application sent successfully'
+            ]);
+
+        $this->assertDatabaseHas('applicants', ['user_id' => $user->id]);
     }
 }
