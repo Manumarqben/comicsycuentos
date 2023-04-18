05f21f43adb82d2a072130857266bddeb1021166
marqben <marqben@hotmail.com>
2023-04-18 16:18:29 +0200

    test: :white_check_mark: Pruebas de borrado del autor

--- tests/Feature/Livewire/Author/DeleteAuthorFormTest.php
+++ tests/Feature/Livewire/Author/DeleteAuthorFormTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature\Livewire\Author;
 
 use App\Http\Livewire\Author\DeleteAuthorForm;
+use App\Models\Author;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
@@ -10,11 +12,64 @@ use Tests\TestCase;
 
 class DeleteAuthorFormTest extends TestCase
 {
+    use RefreshDatabase;
+    use WithFaker;
+
     /** @test */
     public function the_component_can_render()
     {
-        $component = Livewire::test(DeleteAuthorForm::class);
+        $this->actingAs($user = User::factory()->create());
+        $author = Author::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        $this->assertDatabaseHas('authors', ['alias' => $author->alias]);
+
+        $component = Livewire::test(
+            DeleteAuthorForm::class,
+            ['author' => $author]
+        );
 
         $component->assertStatus(200);
+    }
+
+    /** @test */
+    public function a_user_can_delete_your_author_information()
+    {
+        $this->actingAs($user = User::factory()->create());
+        $author = Author::factory()->create([
+            'user_id' => $user->id
+        ]);
+
+        $this->assertDatabaseHas('authors', ['alias' => $author->alias]);
+
+        $component = Livewire::test(
+            DeleteAuthorForm::class,
+            ['author' => $author]
+        )
+            ->call('delete');
+
+        $this->assertDatabaseMissing('authors', ['alias' => $author->alias]);
+    }
+
+    /** @test */
+    public function a_user_cannot_erase_another_author_who_is_not_yours()
+    {
+        $anotherUser = User::factory()->create();
+        $author = Author::factory()->create([
+            'user_id' => $anotherUser->id
+        ]);
+
+        $this->assertDatabaseHas('authors', ['alias' => $author->alias]);
+
+        $this->actingAs($user = User::factory()->create());
+
+        $component = Livewire::test(
+            DeleteAuthorForm::class,
+            ['author' => $author]
+        )
+            ->call('delete');
+
+        $this->assertDatabaseHas('authors', ['alias' => $author->alias]);
     }
 }
