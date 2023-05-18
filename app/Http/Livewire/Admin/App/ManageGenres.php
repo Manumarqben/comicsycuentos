5f88452cec9b1012802cb3d54de170bdfcd7ab04
marqben <marqben@hotmail.com>
2023-05-18 15:01:23 +0200

    feat: :sparkles: Administración de géneros

--- app/Http/Livewire/Admin/App/ManageGenres.php
+++ app/Http/Livewire/Admin/App/ManageGenres.php
@@ -4,6 +4,7 @@ namespace App\Http\Livewire\Admin\App;
 
 use App\Models\Genre;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Validation\Rule;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -13,9 +14,34 @@ class ManageGenres extends Component
     use WithPagination;
 
     public $showDeleteModal;
+    public $showSaveModal;
 
     public Genre $genreTo;
     public $name;
+
+    public function rules()
+    {
+        return [
+            'genreTo.name' => [
+                'required',
+                Rule::unique('genres', 'name')->ignore($this->genreTo->id),
+                'max:70',
+            ],
+            'genreTo.slug' => [
+                'required',
+                Rule::unique('genres', 'slug')->ignore($this->genreTo->id),
+                'max:100',
+            ],
+            'genreTo.description' => [
+                'required',
+                Rule::unique('genres', 'description')->ignore($this->genreTo->id),
+            ],
+        ];
+    }
+
+    protected $validationAttributes = [
+        'slug' => 'name',
+    ];
 
     public function openDeleteModal($id)
     {
@@ -36,6 +62,33 @@ class ManageGenres extends Component
         }
 
         $this->showDeleteModal = false;
+    }
+
+    public function openSaveModal($id = null)
+    {
+        $this->genreTo = Genre::find($id) ?? new Genre();
+        $this->showSaveModal = true;
+    }
+
+    public function save()
+    {
+        $genreId = $this->genreTo->id;
+
+        $this->genreTo->name = trim($this->genreTo->name);
+        $this->genreTo->slug = str($this->genreTo->name)->slug();
+        $this->genreTo->description = trim($this->genreTo->description);
+
+        $this->validate();
+
+        $this->genreTo->save();
+
+        $this->showSaveModal = false;
+
+        if ($genreId) {
+            $this->dispatchBrowserEvent('alert', ['message' => 'Genre updated successfully.']);
+        } else {
+            $this->dispatchBrowserEvent('alert', ['message' => 'Genre created successfully.']);
+        }
     }
 
     public function render()
