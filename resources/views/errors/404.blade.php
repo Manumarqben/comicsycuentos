5965c8500c4aa89169323b7c6d885163b6dfcd7b
marqben <marqben@hotmail.com>
2023-05-28 15:12:55 +0200

    feat: :construction: Error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <div class="container">
+        <div
+            class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <p class="h2">{{ __('Error 404 - Page not found') }}</p>
+            <p>{{ __('We feel it, the page you are looking for has not been found.') }}
+            </p>
+        </div>
+    </div>
+</x-app-layout>
